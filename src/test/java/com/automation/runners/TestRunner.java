52fb26528fa07a9479b132307535f7f73d01fa04
ariansyahriwendi <ariansyahriwendi@gmail.com>
2025-12-15 19:08:00 +0700

    update allure report

--- src/test/java/com/automation/runners/TestRunner.java
+++ src/test/java/com/automation/runners/TestRunner.java
@@ -6,8 +6,11 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"com.automation.hooks", "com.automation.steps"},
-        plugin = {"pretty", "html:target/cucumber-report/cucumber-report.html"},
-        tags = "@Login"
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report/cucumber-report.html",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
+        tags = "@Dashboard"
 )
 
 public class TestRunner extends AbstractTestNGCucumberTests {
